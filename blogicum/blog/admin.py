5f7a7a9c582303dcc00815be6937879dd38daba6
Ilya Kharchenko <kharchenkois2004@gmail.com>
2026-01-18 19:10:27 +0700

    1.01

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -2,12 +2,42 @@ from django.contrib import admin
 from .models import Category, Location, Post
 
 # Register your models here.
-admin.site.register(Category)
-
-
-class CategoryAdmin(admin.ModelAdmin):
-    pass
-
-
 admin.site.register(Location)
-admin.site.register(Post)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'slug',
+        'is_published',
+        'created_at'
+    )
+    prepopulated_fields = {
+        'slug': ('title',)
+    }
+    search_fields = (
+        'title',
+        'description'
+    )
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'author',
+        'category',
+        'location',
+        'is_published',
+        'pub_date'
+    )
+    list_filter = (
+        'is_published',
+        'category',
+        'location',
+        'author'
+    )
+    search_fields = (
+        'title',
+        'text'
+    )
+    date_hierarchy = 'pub_date'
--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -94,6 +94,7 @@ class Post(BaseModel):
     class Meta:
         verbose_name = "публикация"
         verbose_name_plural = "Публикации"
+        ordering = ['-pub_date']
 
     def __str__(self):
         return self.title
